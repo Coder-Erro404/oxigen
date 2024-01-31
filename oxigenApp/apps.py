7af484d88d46f63e8ebb2a9bad5d341cacbb1e55
Coder-Erro404 <kkhansare@gmail.com>
2024-01-31 16:39:53 +0530

    first commit

---
+++ oxigenApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OxigenappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'oxigenApp'
---
+++ oxigenApp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+# --------------- MAIN WEB PAGES -----------------------------
+
+def index(request):
+
+	return render(request, 'index.html')
